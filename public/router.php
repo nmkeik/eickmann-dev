868254fada10606bc963b93b3e859ec4d70da567
max <max@eickmann.com>
2024-09-18 19:45:38 +0200

    added a phpinfo page and configured router to display it correctly

--- public/router.php
+++ public/router.php
@@ -20,6 +20,7 @@ function routeToView($path) {
         die();
     }
     if(!file_exists('../' . $path . 'view.php')) abort();
+    if($path === '/phpinfo/') require 'view.php';
     require '../_components/head.php';
     require '../_components/header.php';
     require 'view.php';
