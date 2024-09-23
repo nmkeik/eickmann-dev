2e3015757f1956e96ea5be6b2627ec27cebcbc78
max <nmk.eickmann@gmail.com>
2024-09-23 12:42:42 +0200

    added Google Analytics

--- public/_components/head.php
+++ public/_components/head.php
@@ -2,6 +2,14 @@
 <html lang="de-DE">
 
     <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-176883191-1"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-176883191-1');
+        </script>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
