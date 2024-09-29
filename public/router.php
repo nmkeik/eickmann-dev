1d9e7d6fc956f2aaa546c94f6d823de643e64224
max <nmk.eickmann@gmail.com>
2024-09-29 22:50:49 +0200

    delted route to phpinfo and chabged word in ourteam uli text

--- public/router.php
+++ public/router.php
@@ -20,7 +20,6 @@ function routeToView($path) {
         die();
     }
     if(!file_exists('../' . $path . 'view.php')) abort();
-    if($path === '/phpinfo/') require 'view.php';
     require '../_components/head.php';
     require '../_components/header.php';
     require 'view.php';
